3cca82d5d1118d4931d78ae3c92c3b01b481a7e0
ricardoo-azevedo <ricardoazevedo1911@gmail.com>
2026-04-04 18:22:36 -0300

    feat: adicionando anotação em guicheRepository

--- mobileTicketsSpring/src/main/java/com/ricardo/mobileTicketsSpring/repositorys/GuicheRepository.java
+++ mobileTicketsSpring/src/main/java/com/ricardo/mobileTicketsSpring/repositorys/GuicheRepository.java
@@ -4,10 +4,12 @@ import java.util.Optional;
 import java.util.UUID;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.ricardo.mobileTicketsSpring.models.GuicheModel;
 import java.util.List;
 
+@Repository
 public interface GuicheRepository extends JpaRepository<GuicheModel, UUID> {
 
   Optional<GuicheModel> findByNome(String nome);
@@ -15,7 +17,5 @@ public interface GuicheRepository extends JpaRepository<GuicheModel, UUID> {
   boolean existsByNome(String nome);
 
   List<GuicheModel> findByNomeContaining(String nome);
-
-
 
 }
